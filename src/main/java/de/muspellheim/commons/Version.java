d1f5bce046a828ab67d031e3148f16a05bd63567
Falko Schumann <falko.schumann@muspellheim.de>
2019-10-17 23:14:52 +0200

    Version is comparable

--- src/main/java/de/muspellheim/commons/Version.java
+++ src/main/java/de/muspellheim/commons/Version.java
@@ -13,14 +13,12 @@ import lombok.*;
 /**
  * A version according to <a href="https://semver.org">Semantic Versioning</a>.
  * <p>
- * Pre release and build metadata are optional and can be null.
+ * Pre release and build metadata are optional and can be <code>null</code>.
  */
 @Value
 @RequiredArgsConstructor(staticName = "of")
 @SuppressWarnings("checkstyle:VisibilityModifier")
-public class Version {
-
-    // TODO Implement Comparable
+public class Version implements Comparable<Version> {
 
     private static final Pattern PATTERN = Pattern.compile("^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)"
         + "(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?"
@@ -63,6 +61,64 @@ public class Version {
         return major + "." + minor + "." + patch
             + (preRelease != null ? " " + preRelease : "")
             + (buildMetadata != null ? " (" + buildMetadata + ")" : "");
+    }
+
+    @Override
+    public int compareTo(@NonNull Version o) {
+        if (this == o) {
+            return 0;
+        }
+
+        int result = Integer.compare(major, o.major);
+        if (result != 0) {
+            return result;
+        }
+
+        result = Integer.compare(minor, o.minor);
+        if (result != 0) {
+            return result;
+        }
+
+        result = Integer.compare(patch, o.patch);
+        if (result != 0) {
+            return result;
+        }
+
+        if (preRelease == o.preRelease) {
+            return 0;
+        }
+        if (preRelease == null) {
+            return 1;
+        }
+        if (o.preRelease == null) {
+            return -1;
+        }
+        return comparePreRelease(preRelease, o.preRelease);
+    }
+
+    private static int comparePreRelease(String pr1, String pr2) {
+        int result;
+        String[] pr1a = pr1.split("\\.");
+        String[] pr2a = pr2.split("\\.");
+        int lim = Math.min(pr1a.length, pr2a.length);
+        for (int i = 0; i < lim; i++) {
+            String a = pr1a[i];
+            String b = pr2a[i];
+            try {
+                int a1 = Integer.parseInt(a);
+                int b1 = Integer.parseInt(b);
+                result = Integer.compare(a1, b1);
+                if (result != 0) {
+                    return result;
+                }
+            } catch (NumberFormatException ignored) {
+                result = a.compareTo(b);
+                if (result != 0) {
+                    return result;
+                }
+            }
+        }
+        return pr1a.length - pr2a.length;
     }
 
 }
--- src/test/java/de/muspellheim/commons/VersionTests.java
+++ src/test/java/de/muspellheim/commons/VersionTests.java
@@ -56,4 +56,59 @@ class VersionTests {
         assertEquals("text is not a version", e.getMessage());
     }
 
+    @Test
+    void compare() {
+        Version a = Version.of(1, 0, 0);
+        Version a1 = Version.of(1, 0, 0);
+        Version b = Version.of(2, 0, 0);
+        Version c = Version.of(2, 1, 0);
+        Version d = Version.of(2, 1, 1);
+
+        assertAll(
+            () -> assertEquals(0, a.compareTo(a1), "1.0.0 = 1.0.0"),
+            () -> assertTrue(a.compareTo(b) < 0, "1.0.0 < 2.0.0"),
+            () -> assertTrue(b.compareTo(c) < 0, "2.0.0 < 2.1.0"),
+            () -> assertTrue(c.compareTo(d) < 0, "2.1.0 < 2.1.1")
+        );
+    }
+
+    @Test
+    void compareWithNull() {
+        // Given
+        Version v = Version.of(1, 0, 0);
+
+        // When
+        assertThrows(NullPointerException.class, () -> v.compareTo(null));
+    }
+
+    @Test
+    void withoutPreReleaseIsNewer() {
+        Version a = Version.parse("1.0.0-alpha");
+        Version b = Version.parse("1.0.0");
+
+        assertTrue(a.compareTo(b) < 0, "1.0.0-alpha < 1.0.0");
+    }
+
+    @Test
+    void comparePreReleases() {
+        Version a = Version.parse("1.0.0-alpha");
+        Version b = Version.parse("1.0.0-alpha.1");
+        Version c = Version.parse("1.0.0-alpha.beta");
+        Version d = Version.parse("1.0.0-beta");
+        Version e = Version.parse("1.0.0-beta.2");
+        Version f = Version.parse("1.0.0-beta.11");
+        Version g = Version.parse("1.0.0-rc.1");
+        Version h = Version.parse("1.0.0");
+
+        assertAll(
+            () -> assertTrue(a.compareTo(b) < 0, "1.0.0-alpha < 1.0.0-alpha.1"),
+            () -> assertTrue(b.compareTo(c) < 0, "1.0.0-alpha.1 < 1.0.0-alpha.beta"),
+            () -> assertTrue(c.compareTo(d) < 0, "1.0.0-alpha.beta < 1.0.0-beta"),
+            () -> assertTrue(d.compareTo(e) < 0, "1.0.0-beta < 1.0.0-beta.2"),
+            () -> assertTrue(e.compareTo(f) < 0, "1.0.0-beta.2 < 1.0.0-beta.11"),
+            () -> assertTrue(f.compareTo(g) < 0, "1.0.0-beta.11 < 1.0.0-rc.1"),
+            () -> assertTrue(g.compareTo(h) < 0, "1.0.0-rc.1 < 1.0.0")
+        );
+    }
+
 }
